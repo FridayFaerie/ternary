8043c7ff519a26b1ec82fc2734e309c7ae7acbde
Friday <faeriethe@gmail.com>
2024-06-21 23:38:13 +0800

    re-organised everything

--- main.py
+++ main.py
@@ -1,83 +1,34 @@
-#  id: [type,       inputs, outputs         ]
-circuit = {
-    0: ["input",    [1,-1], [(1,0),(2,0)]   ],
-    1: ["split",    [2],    [(3,0),(4,0)]   ],
-    2: ["split",    [2],    [(3,1),(4,1)]   ],
-    3: ["min",      [2,2],  [(6,0)]         ],
-    4: ["max",      [2,2],  [(5,0)]         ],
-    5: ["neg",      [2],    [(6,1)]         ],
-    6: ["max",      [2,2],  [(7,0)]         ],
-    7: ["out",      [2],    []              ]
-    }
-custom = {
-    "pos": {'0':[1],'+':[1],'-':[2]}
-}
-tasklist = []
+import pygame, sys
+clock = pygame.time.Clock()
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('Ternary Simulator')
 
-def process(component):
-    gate,inputs,destinations = component
-    match gate:
-        case "input":
-            size = len(inputs)
-            outputs = [None] * size
-            for i in range(size):
-                outputs[i] = inputs[i]
+WINDOW_SIZE = (600,400)
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
+display = pygame.Surface((300,200))
 
-        case "neg":
-            outputs = [inputs[0]*-1]
 
-        case "max":
-            outputs = [max(inputs[0],inputs[1])]
+
+
+
+while True:
+    display.fill((146,244,255))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                continue
+
+    g
+
+
         
-        case "min":
-            outputs = [min(inputs[0],inputs[1])]
-        
-        case "split":
-            outputs = [inputs[0]] * len(destinations)
 
-        case "out":
-            outputs = []
-            print(outputs)
+    screen.blit(pygame.transform.scale(display, WINDOW_SIZE),(0,0))
+    pygame.display.update()
+    clock.tick(60)
 
-        case other:
-            if gate[1] in custom:
-                inputstring = ''
-                for input in inputs:
-                    match input:
-                        case 1:
-                            inputstring += '+'
-                        case -1:
-                            inputstring += '-'
-                        case 0:
-                            inputstring += '0'
-                outputs = custom[gate[1]][inputstring]
-
-            else:
-                print(f"gate {gate} not found")
-                #TODO: finding gate from file
-                print("finding gates from file not implemented")
-                exit()
-
-
-    for i in range(len(outputs)):
-        destination = destinations[i]
-        value = outputs[i]
-
-        if circuit[destination[0]][1][destination[1]] == value:
-            continue
-        circuit[destination[0]][1][destination[1]] = value
-        if destination[0] not in tasklist:
-            tasklist.append(destination[0])
-
-
-process(circuit[0])
-counter = 0
-
-while tasklist != []:
-    process(circuit[tasklist[0]])
-    tasklist.pop(0)
-
-    counter += 1
-    if counter>100:
-        print("depth reached")
-        tasklist = []
